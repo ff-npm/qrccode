e8093329b24844bec895ca838041fbe9a7961f32
dxj <hanyuan_company_dxj@dxj.com>
2019-07-16 17:40:01 +0800

    ios扫码闪光灯关闭

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import QrcodeView from "./QrcodeView";
 import { NativeModules } from 'react-native';
-const { RNQRCode,QrCodeView } = NativeModules;
+const { RNQRCode, QrCodeView } = NativeModules;
 RNQRCode.initQrCodeView = QrCodeView.initScanQRCodeWithCallback;
 export default RNQRCode;
 module.exports = {
