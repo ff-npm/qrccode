1c70270abf1938b14630e95435ef02f1fc1d500e
dxj <hanyuan_company_dxj@dxj.com>
2019-07-19 11:29:03 +0800

    跳转权限设置页

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import QrcodeView from "./QrcodeView";
 import { NativeModules } from 'react-native';
 const { RNQRCode, QrCodeView } = NativeModules;
 RNQRCode.initQrCodeView = QrCodeView.initScanQRCodeWithCallback;
+RNQRCode.flashSwitch = QrCodeView.flashSwitch;
+RNQRCode.authJump = QrCodeView.authJump;
 export default RNQRCode;
 module.exports = {
     QrcodeView,
